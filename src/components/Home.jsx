921aeb8fc1b6cdf464e95997f8d8a93e493ec0b6
khushbunakhat <nakhatkhushbu@gmail.com>
2022-01-14 19:34:08 +0530

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,26 +17,26 @@ function Home(){
                 <p>I am fitness trainer.My mission is to cut through the noise in the health and fitness industry .</p>
             </div>
             <div className={styles.logo}>
+                
+                <img width="50px"src="https://cdn-icons-png.flaticon.com/512/174/174857.png"alt="linkedin" />   
+            </div>
+            <div className={styles.logo}>
                 <img width="50px"src="https://cdn-icons-png.flaticon.com/512/1409/1409946.png"alt="insta" />    
             </div>
 
             <div className={styles.logo}>
           
-                <img width="50px"src="https://cdn-icons.flaticon.com/png/512/2504/premium/2504903.png?token=exp=1642161886~hmac=460359cbef4dca255cd815dc990f0a87"alt="facebook" />
+                <img width="50px"src="https://cdn-icons-png.flaticon.com/512/733/733547.png"alt="facebook" />
                 
             </div>
 
             <div className={styles.logo}>
                
-                <img width="50px"src="https://cdn-icons.flaticon.com/png/512/3938/premium/3938026.png?token=exp=1642161729~hmac=d5e4a2155c8c8eea2204c174fea1e3b6"alt="youtube" />
+                <img width="50px"height="60px"src="https://cdn-icons-png.flaticon.com/512/1384/1384060.png"alt="youtube" />
                 
             </div>
 
-            <div className={styles.logo}>
-                
-                <img width="50px"src="https://cdn-icons-png.flaticon.com/512/174/174857.png"alt="linkedin" />
-                
-            </div>
+           
             </div>
             <div className={styles.rightbox}>
         <Video/>
