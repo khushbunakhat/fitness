a5b45333a11ec0dd6a40d00b365ca8dc0d44e237
khushbunakhat <nakhatkhushbu@gmail.com>
2022-05-05 12:52:10 +0530

    updated

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import styles from "./styles.module.css";
-import Data from "./Data.json"
 import ReactPlayer from 'react-player';
 import Draggable from "react-draggable";
 
-function Video(props){
-      
+function Video(props){    
     return(
         <>
           <h1>Yoga-Videos</h1>
           {props.name && props.name.map((video)=>{
-               
-             
               return(
                   <Draggable>
               <div className={styles.videobox} key={video.id} >
@@ -23,7 +19,6 @@ function Video(props){
               )
           })}
         </>
-      
     )
 }
 export default Video;
